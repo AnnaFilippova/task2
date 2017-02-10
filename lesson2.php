b1cedd78fb6f669f6fe275ccfadd1bd89e2e8524
Anna Filippova <filippova_22@mail.ru>
2017-02-10 20:26:20 +0400

    done!

---
+++ lesson2.php
@@ -0,0 +1,22 @@
+<?php
+  $sx = rand(0, 100);
+  echo "selected number: " + $sx;
+  $var1 = 1;
+  $var2 = 1;
+
+  while (true) {
+    if ($var1 > $sx) {
+      echo "задуманное число не входит в числовой ряд <br/>";
+      return;
+    } else {
+      if ($var1 == $sx) {
+        echo "задуманное число входит в числовой ряд <br/>";
+        return;
+      } else {
+        $var3 = $var1;
+        $var1 += $var2;
+        $var2 = $var3;
+      }
+    }
+  }
+?>
